6a82d29244c943bb1f368f3d8cda07b2e6287b6c
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2019-10-26 15:57:01 +0900

    投稿の作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,11 @@
-use Illuminate\Support\Str;
-
 @extends('layout')
 
 @section('content')
+<div class="bm4">
+  <a href="{{ route('posts.create') }}" class="btn btn-primary">
+    投稿を新規作成する
+  </a>
+</div>
 <div class="container mt-4">
   @foreach ($posts as $post)
     <div class="card mb-4">
@@ -11,7 +14,7 @@ use Illuminate\Support\Str;
       </div>
       <div class="card-body">
         <p class="card-text">
-          {{!! nl2br(e(Str::limit($post->body, 200))) !!}}
+          {!! nl2br(e(Str::limit($post->body, 200))) !!}
         </p>
       </div>
       <div class="card-footer">
