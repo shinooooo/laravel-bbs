44510d40909ca1571011e96da27064da22b9cf53
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2019-10-27 12:08:55 +0900

    bladeの編集

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,19 +7,19 @@
         投稿の新規作成
       </h1>
 
-      <form method="POST" action="{{ route('posts.store) }}">
+      <form method="POST" action="{{ route('posts.store') }}">
         {{ csrf_field() }}
         <fieldset class="mb-4">
           <div class="form-group">
-            <label for="form-group">
+            <label for="title">
               タイトル
             </label>
             <input
-              id="title"
-              name="title"
-              class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}"
-              value="{{ old('title') }}"
-              type="text"
+            id="title"
+            name="title"
+            class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}"
+            value="{{ old('title') }}"
+            type="text"
             >
             @if ($errors->has('title'))
               <div class="invalid-feedback">
@@ -32,14 +32,12 @@
             <label for="body">
               本文
             </label>
-
             <textarea
             id="body"
             name="body"
-            class="form-control {{$error->has('body') ? 'is-invalid' : '' }}"
+            class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
             rows="4"
-            >{{ old('body') }}
-            </textarea>
+            >{{ old('body') }}</textarea>
           @if ($errors->has('body'))
             <div class="invalid-feedback">
               {{ $errors->first('body') }}
